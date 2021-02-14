abc4cb1df13130896a25d38eedd458bb5ad9882e
fkzwd <lives352@gmail.com>
2021-02-14 14:35:30 +0300

    refactor main. add -D args [delay,sleep,count]

--- src/main/java/com/vk/dwzkf/anya/chooser/Application.java
+++ src/main/java/com/vk/dwzkf/anya/chooser/Application.java
@@ -13,16 +13,34 @@ public class Application {
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
+        String sleepTimeStr = System.getProperty("delay");
+        String timesStr = System.getProperty("repeat");
+        String variantsCountStr = System.getProperty("count");
+
         for (int i = 0; i < 80; i++) {
             System.out.println();
         }
         Console.clear();
         String whatWeFinding = readStr("What we finding: ");
-        int times = readInt("repeat times");
-        int sleepTime = readInt("sleep time");
-        int filmsSize = readInt("variants count");
+
+        int times;
+        int sleepTime;
+        if (sleepTimeStr == null && timesStr == null) {
+            times = readInt("repeat times");
+            sleepTime = readInt("sleep time");
+        } else {
+            times = Integer.parseInt(timesStr);
+            sleepTime = Integer.parseInt(sleepTimeStr);
+        }
+
+        int variantsCount;
+        if (variantsCountStr == null) {
+            variantsCount = readInt("variants count");
+        } else {
+            variantsCount = Integer.parseInt(variantsCountStr);
+        }
         List<String> films = new ArrayList<>();
-        for (int i = 0; i < filmsSize; i++) {
+        for (int i = 0; i < variantsCount; i++) {
             String str = readString(whatWeFinding+" ",i);
             films.add(str);
         }
